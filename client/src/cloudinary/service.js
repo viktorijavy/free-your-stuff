b62dcfdf900fd2b0af5082af20b05abb41393db4
Viktorija Kuneviciute <viktorija.qun@gmail.com>
2021-12-15 18:22:16 +0100

    optimised cloudinary service for deployment

--- client/src/cloudinary/service.js
+++ client/src/cloudinary/service.js
@@ -1,38 +1,37 @@
 import axios from "axios";
 
-const service = axios.create({
-  // make sure you use PORT = 5005 (the port where our server is running)
-  baseURL: "http://localhost:5005",
-  withCredentials: true // => you might need this option if using cookies and sessions
-});
+// const service = axios.create({
+//   // make sure you use PORT = 5005 (the port where our server is running)
+  
+//   withCredentials: true // => you might need this option if using cookies and sessions
+// });
 
 const errorHandler = err => {
   throw err;
 };
 
 const getItems = () => {
-  return service
+  return axios
     .get("/items")
     .then((res) => res.data)
     .catch(errorHandler);
 };
 
 const uploadImage = (file) => {
-  return service
+  return axios
     .post("/upload", file)
     .then(res => res.data)
     .catch(errorHandler);
 };
 
 const createItem = (newItem) => {
-  return service
+  return axios
     .post("/items", newItem)
     .then(res => res.data)
     .catch(errorHandler);
 };
 
 const cloudinaryService = {
-    service,
     getItems,
     uploadImage,
     createItem
