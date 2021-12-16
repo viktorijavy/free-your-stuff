09562978be3259a7d9d2dd80376bfd9cdc7e48c1
Viktorija Kuneviciute <viktorija.qun@gmail.com>
2021-12-16 14:36:48 +0100

    fixed some routing

--- client/src/cloudinary/service.js
+++ client/src/cloudinary/service.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// const service = axios.create({
-//   // make sure you use PORT = 5005 (the port where our server is running)
-//   baseURL: "http://localhost:5005",
-//   withCredentials: true // => you might need this option if using cookies and sessions
-// });
+const service = axios.create({
+  // make sure you use PORT = 5005 (the port where our server is running)
+  baseURL: "http://localhost:5005",
+  withCredentials: true // => you might need this option if using cookies and sessions
+});
 
 const errorHandler = err => {
   throw err;
@@ -12,29 +12,31 @@ const errorHandler = err => {
 
 const getItems = () => {
   return axios
-    .get("/items")
+    .get("/api/items")
     .then((res) => res.data)
     .catch(errorHandler);
 };
 
 const uploadImage = (file) => {
   return axios
-    .post("/upload", file)
+    .post("/api/upload", file)
     .then(res => res.data)
     .catch(errorHandler);
 };
 
 const createItem = (newItem) => {
   return axios
-    .post("/items", newItem)
+    .post("/api/items", newItem)
     .then(res => res.data)
     .catch(errorHandler);
 };
 
 const cloudinaryService = {
+  service,
     getItems,
     uploadImage,
     createItem,
+    
   };
   
 export default cloudinaryService;
